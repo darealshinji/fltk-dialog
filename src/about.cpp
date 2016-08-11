bab837d7e3f1c8d97342cc5badfe710fe7a3eccc
darealshinji <djcj@gmx.de>
2016-08-11 19:43:58 +0200

    New application icon (taken from Haiku OS)

--- src/about.cpp
+++ src/about.cpp
@@ -65,8 +65,8 @@ int about()
   Fl_Box    *box;
   Fl_Button *but_lic, *but_close;
 
-  int winw = 430;
-  int winh = 400;
+  int winw = 450;
+  int winh = 420;
   int bord = 10;
   int butw = 100;
   int buth = 26;
@@ -74,15 +74,22 @@ int about()
   std::string getver = get_fltk_version();
   std::string about_text = "\n"
     "- FLTK dialog -\n"
-    "run dialog boxes from shell scripts\n\n"
+    "run dialog boxes from shell scripts\n"
+    "\n"
     "Using FLTK version " + getver + "\n"
-    "http://www.fltk.org\n\n"
+    "http://www.fltk.org\n"
+    "\n"
     "Copyright \xc2\xa9 2016 djcj <djcj@gmx.de>\n"
-    "https://github.com/darealshinji/fltk-dialog\n\n"
+    "https://github.com/darealshinji/fltk-dialog\n"
+    "\n"
     "The FLTK library and logo are\n"
     "copyright \xc2\xa9 1998-2016 by Bill Spitzak and others.\n"
     "The calendar widget is copyright \xc2\xa9 1999-2000\n"
-    "by the Flek development team.";
+    "by the Flek development team."
+#ifdef WITH_ICON
+    "\nThe application icon is copyright \xc2\xa9 2016 by Haiku, Inc."
+#endif
+    /**/;
   const char *about_text_c = about_text.c_str();
 
   win = new Fl_Window(winw, winh, "About FLTK dialog");
@@ -122,7 +129,7 @@ void license()
     /* sed 's|"|\\"|g; s|^|    "|g; s|$|\\n"|g' LICENSE */
     "The MIT License (MIT)\n"
     "\n"
-    "Copyright (c) 2016, djcj <djcj@gmx.de>\n"
+    "Copyright (c) 2016  djcj <djcj@gmx.de>\n"
     "\n"
     "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
     "of this software and associated documentation files (the \"Software\"), to deal\n"
@@ -144,7 +151,7 @@ void license()
     "\n"
     "\n"
     "\n"
-    "Copyright (c) 1998-2016 by Bill Spitzak and others.\n"
+    "Copyright (c) 1998-2016  Bill Spitzak and others.\n"
     "\n"
     "The FLTK library and included programs are provided under the terms of the\n"
     "GNU Library General Public License (LGPL) with the following exceptions:\n"
@@ -188,7 +195,7 @@ void license()
     "\n"
     "\n"
     "\n"
-    "Copyright (c) 1999-2000 by the Flek development team.\n"
+    "Copyright (c) 1999-2000  the Flek development team.\n"
     "\n"
     "This library is free software; you can redistribute it and/or\n"
     "modify it under the terms of the GNU Library General Public\n"
@@ -204,6 +211,32 @@ void license()
     "License along with this library; if not, write to the Free Software\n"
     "Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307\n"
     "USA.\n"
+#ifdef WITH_ICON
+    "\n"
+    "\n"
+    "\n"
+    "The MIT License (MIT)\n"
+    "\n"
+    "Copyright (c) 2016  Haiku, Inc.\n"
+    "\n"
+    "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    "of this software and associated documentation files (the \"Software\"), to deal\n"
+    "in the Software without restriction, including without limitation the rights\n"
+    "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+    "copies of the Software, and to permit persons to whom the Software is\n"
+    "furnished to do so, subject to the following conditions:\n"
+    "\n"
+    "The above copyright notice and this permission notice shall be included in all\n"
+    "copies or substantial portions of the Software.\n"
+    "\n"
+    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
+    "SOFTWARE.\n"
+#endif  /* WITH_ICON */
   );
   but_close = new Fl_Button((winw-butw)/2, winh-buth-bord, butw, buth, fl_close);
   but_close->callback(about_hide_cb);
