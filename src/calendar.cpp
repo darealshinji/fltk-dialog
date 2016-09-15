0a43692043a05a22f6dc655b5134dfc083614eb1
darealshinji <djcj@gmx.de>
2016-09-15 16:53:39 +0200

    calendar: remove getFormattedDate(), support \t and \n as FORMAT

--- src/calendar.cpp
+++ src/calendar.cpp
@@ -39,26 +39,7 @@
 #include "fltk-dialog.hpp"
 
 
-Fl_Calendar *cal;
-
-/* must be global */
-char formattedDate[255];
-
-char *getFormattedDate(int y, int m, int d, char *fmt)
-{
-  /* based on strptime(3) example code */
-
-  struct tm tm;
-  char buf[255];
-  char date[31];
-
-  sprintf(date, "%d-%d-%d", y, m, d);
-  memset(&tm, 0, sizeof(struct tm));
-  strptime(date, "%Y-%m-%d", &tm);
-  strftime(buf, sizeof(buf), fmt, &tm);
-  strncpy(formattedDate, buf, sizeof(buf));
-  return formattedDate;
-}
+Fl_Calendar *calendar;
 
 static void calendar_ok_cb(Fl_Widget *w)
 {
@@ -68,7 +49,7 @@ static void calendar_ok_cb(Fl_Widget *w)
 
 static void calendar_cancel_cb(Fl_Widget*)
 {
-  delete cal;
+  delete calendar;
   exit(1);
 }
 
@@ -87,8 +68,10 @@ int dialog_fl_calendar(       char *calendar_title,
   int buth = 26;
   int butw = winw/2-(bord*3)/2;  /* 107 */
 
-  int ret, y, m, d;
-  char *fmtc, *date_formatted;
+  struct tm time;
+  int ret;
+  char tmp[31];
+  char date[255];
 
   if (calendar_title == NULL)
   {
@@ -103,7 +86,9 @@ int dialog_fl_calendar(       char *calendar_title,
   {
     /* glibc date formats
      * example date: 2006-01-08 */
-    fmt = repstr(fmt, "%", "%%");   /* literal %; add first! */
+    fmt = repstr(fmt, "\\n", "\n"); /* newline */
+    fmt = repstr(fmt, "\\t", "\t"); /* tab */
+    fmt = repstr(fmt, "%", "%%");   /* literal % */
     fmt = repstr(fmt, "d", "%-d");  /* day (8) */
     fmt = repstr(fmt, "D", "%d");   /* day (08) */
     fmt = repstr(fmt, "m", "%-m");  /* month (1) */
@@ -122,7 +107,7 @@ int dialog_fl_calendar(       char *calendar_title,
   }
 
   win = new Fl_Window(winw, winh, calendar_title);
-  cal = new Fl_Calendar(bord, bord, calw, calw);
+  calendar = new Fl_Calendar(bord, bord, calw, calw);
   win->begin();
   win->callback(calendar_cancel_cb);  /* exit(1) */
   {
@@ -136,14 +121,14 @@ int dialog_fl_calendar(       char *calendar_title,
 
   ret = Fl::run();
 
-  y = cal->year();
-  m = cal->month();
-  d = cal->day();
-  fmtc = (char *)fmt.c_str();
-  date_formatted = getFormattedDate(y, m, d, fmtc);
-  std::cout << date_formatted << std::endl;
+  sprintf(tmp, "%d-%d-%d", calendar->year(), calendar->month(), calendar->day());
+  memset(&time, 0, sizeof(struct tm));
+  strptime(tmp, "%Y-%m-%d", &time);
+  strftime(date, sizeof(date), fmt.c_str(), &time);
 
-  delete cal;
+  std::cout << date << std::endl;
+
+  delete calendar;
   return ret;
 }
 
