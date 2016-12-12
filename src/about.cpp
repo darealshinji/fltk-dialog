f1014c07b24da29fe399aba6c0a9f0774bc64ac8
darealshinji <djcj@gmx.de>
2016-12-12 15:18:04 +0100

    About: delete 'patches' tab

--- src/about.cpp
+++ src/about.cpp
@@ -38,9 +38,6 @@
 #include "fltk.xpm"
 #include "fltk-dialog.hpp"
 
-//#define PATCHES_TAB
-#include "about.hpp"
-
 
 static Fl_Double_Window *about_win;
 
@@ -73,6 +70,121 @@ static std::string about_text = "\n"
     "\n"
 #endif
 /* about_text end */;
+
+/* sed 's|"|\\"|g; s|^|    "|g; s|$|\\n"|g' LICENSE */
+static const char *license_buffer_text =
+    "The MIT License (MIT)\n"
+    "\n"
+    "Copyright (c) 2016  djcj <djcj@gmx.de>\n"
+    "\n"
+    "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    "of this software and associated documentation files (the \"Software\"), to deal\n"
+    "in the Software without restriction, including without limitation the rights\n"
+    "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+    "copies of the Software, and to permit persons to whom the Software is\n"
+    "furnished to do so, subject to the following conditions:\n"
+    "\n"
+    "The above copyright notice and this permission notice shall be included in all\n"
+    "copies or substantial portions of the Software.\n"
+    "\n"
+    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
+    "SOFTWARE.\n"
+    "\n"
+    "\n"
+    "\n"
+    "Copyright (c) 1998-2016  Bill Spitzak and others.\n"
+    "\n"
+    "The FLTK library and included programs are provided under the terms of the\n"
+    "GNU Library General Public License (LGPL) with the following exceptions:\n"
+    "\n"
+    "1. Modifications to the FLTK configure script, config header file, and\n"
+    "makefiles by themselves to support a specific platform do not constitute a\n"
+    "modified or derivative work.\n"
+    "The authors do request that such modifications be contributed to the FLTK\n"
+    "project - send all contributions to \"fltk-bugs@fltk.org\".\n"
+    "\n"
+    "2. Widgets that are subclassed from FLTK widgets do not constitute a\n"
+    "derivative work.\n"
+    "\n"
+    "3. Static linking of applications and widgets to the FLTK library does not\n"
+    "constitute a derivative work and does not require the author to provide\n"
+    "source code for the application or widget, use the shared FLTK libraries, or\n"
+    "link their applications or widgets against a user-supplied version of FLTK.\n"
+    "If you link the application or widget to a modified version of FLTK, then\n"
+    "the changes to FLTK must be provided under the terms of the LGPL in sections\n"
+    "1, 2, and 4.\n"
+    "\n"
+    "4. You do not have to provide a copy of the FLTK license with programs that\n"
+    "are linked to the FLTK library, nor do you have to identify the FLTK license\n"
+    "in your program or documentation as required by section 6 of the LGPL.\n"
+    "However, programs must still identify their use of FLTK. The following example\n"
+    "statement can be included in user documentation to satisfy this requirement:\n"
+    "[program/widget] is based in part on the work of the FLTK project (http://www.fltk.org).\n"
+    "\n"
+    "    This library is free software; you can redistribute it and/or\n"
+    "    modify it under the terms of the GNU Library General Public\n"
+    "    License as published by the Free Software Foundation; either\n"
+    "    version 2 of the License, or (at your option) any later version.\n"
+    "\n"
+    "    This library is distributed in the hope that it will be useful,\n"
+    "    but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+    "    Library General Public License for more details.\n"
+    "\n"
+    "    You should have received a copy of the GNU Library General Public License\n"
+    "    along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
+    "\n"
+    "\n"
+    "\n"
+    "Copyright (c) 1999-2000  the Flek development team\n"
+    "Copyright (c) 2016  djcj <djcj@gmx.de>\n"
+    "\n"
+    "This library is free software; you can redistribute it and/or\n"
+    "modify it under the terms of the GNU Library General Public\n"
+    "License as published by the Free Software Foundation; either\n"
+    "version 2 of the License, or (at your option) any later version.\n"
+    "\n"
+    "This library is distributed in the hope that it will be useful,\n"
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+    "Library General Public License for more details.\n"
+    "\n"
+    "You should have received a copy of the GNU Library General Public\n"
+    "License along with this library; if not, write to the Free Software\n"
+    "Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307\n"
+    "USA.\n"
+#ifdef WITH_DEFAULT_ICON
+    "\n"
+    "\n"
+    "\n"
+    "The MIT License (MIT)\n"
+    "\n"
+    "Copyright (c) 2016  Haiku, Inc.\n"
+    "\n"
+    "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    "of this software and associated documentation files (the \"Software\"), to deal\n"
+    "in the Software without restriction, including without limitation the rights\n"
+    "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+    "copies of the Software, and to permit persons to whom the Software is\n"
+    "furnished to do so, subject to the following conditions:\n"
+    "\n"
+    "The above copyright notice and this permission notice shall be included in all\n"
+    "copies or substantial portions of the Software.\n"
+    "\n"
+    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
+    "SOFTWARE.\n"
+#endif  /* WITH_ICON */
+/* license_buffer_text end */;
 
 class uri_box : public Fl_Box
 {
@@ -134,11 +246,6 @@ int about()
   Fl_Text_Display  *license_display;
   uri_box          *uri_button1, *uri_button2;
   Fl_Return_Button *but_close;
-#ifdef PATCHES_TAB
-  Fl_Group *g_patches;
-  Fl_Text_Buffer *patches_buffer;
-  Fl_Text_Display *patches_display;
-#endif
 
   const char *uri1 = "http://www.fltk.org";
   const char *uri2 = "https://github.com/darealshinji/fltk-dialog";
@@ -180,19 +287,6 @@ int about()
         license_buffer->text(license_buffer_text);
       }
       g_license->end();
-
-#ifdef PATCHES_TAB
-      g_patches = new Fl_Group(10, 40, 430, 404, "Patches");
-      {
-        patches_buffer = new Fl_Text_Buffer();
-        patches_display = new Fl_Text_Display(10, 40, 430, 404);
-        patches_display->textfont(FL_COURIER);
-        patches_display->buffer(patches_buffer);
-        patches_buffer->text(patches_buffer_text);
-      }
-      g_patches->end();
-#endif
-
     }
     about_tab->end();
 
