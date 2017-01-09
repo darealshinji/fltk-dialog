36d5eeca2f3b21a02561d2e9439b7bc4e65c36b3
darealshinji <djcj@gmx.de>
2017-01-09 02:19:39 +0100

    Bump copyright year in license buffer text

--- src/about.cpp
+++ src/about.cpp
@@ -75,7 +75,7 @@ static std::string about_text = "\n"
 static const char *license_buffer_text =
     "The MIT License (MIT)\n"
     "\n"
-    "Copyright (c) 2016  djcj <djcj@gmx.de>\n"
+    "Copyright (c) 2016-2017  djcj <djcj@gmx.de>\n"
     "\n"
     "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
     "of this software and associated documentation files (the \"Software\"), to deal\n"
