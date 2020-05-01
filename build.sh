64865d3db3789443685be91b7c9f23ea7cbf9ea1
darealshinji <djcj@gmx.de>
2020-05-01 18:45:44 +0200

    fix build

--- build.sh
+++ build.sh
@@ -46,13 +46,13 @@ if [ ! -e build/fltk/lib/libfltk_images.a ]; then
     -DCMAKE_C_FLAGS="$DEF_CFLAGS" \
     -DOPTION_USE_GL="OFF" \
     -DOPTION_BUILD_EXAMPLES="OFF" \
-    -DOPTION_PRINT_SUPPORT="OFF" \
     -DOPTION_USE_NANOSVG="ON" \
     -DOPTION_USE_PANGO="ON" \
     -DOPTION_CAIRO="OFF" \
     -DOPTION_USE_SYSTEM_LIBJPEG="$syslibs" \
     -DOPTION_USE_SYSTEM_LIBPNG="$syslibs" \
     -DOPTION_USE_SYSTEM_ZLIB="$syslibs"
+    #-DOPTION_PRINT_SUPPORT="OFF"
   make -j$JOBS fltk_images
   cd -
 fi
@@ -72,7 +72,7 @@ V=1 \
 USE_EXTERNAL_PLUGINS="$external_plugins" \
 USE_DLOPEN="$use_dlopen" \
 CXXFLAGS="$DEF_CXXFLAGS -I$PWD/fltk -I$PWD/../fltk $(./fltk/bin/fltk-config --use-images --cxxflags) $define_git_hash" \
-LDFLAGS="$DEF_LDFLAGS -L$PWD/fltk/lib $(./fltk/bin/fltk-config --use-images --ldflags) -lmagic" \
+LDFLAGS="$DEF_LDFLAGS -L$PWD/fltk/lib $(./fltk/bin/fltk-config --use-images --ldflags) $(pkg-config --libs pangoxft) -lmagic" \
 QT_CXXFLAGS="$DEF_CXXFLAGS $(pkg-config --cflags Qt5Widgets Qt5Core)" \
 QT_LDFLAGS="$DEF_LDFLAGS $(pkg-config --libs Qt5Widgets Qt5Core)" \
 BUILDDIR="$PWD/fltk_dialog" \
@@ -89,18 +89,4 @@ cd fltk
 patch -p1 -R < ../fltk_patches.diff
 rm patches_applied_stamp
 
-
-### bundling ###
-exit 0
-if [ "x$1" = "x--disable-dlopen" ]; then
-  cd ../build
-
-  cp -f ../src/icon.png fltk-dialog.png
-  mkdir -p AppDir/usr/share/file
-  cp -f /usr/share/file/magic.mgc AppDir/usr/share/file
-
-  wget -c https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-  chmod a+x *.AppImage
-  ./linuxdeploy-x86_64.AppImage -oappimage --appdir=AppDir --create-desktop-file -ifltk-dialog.png -efltk_dialog/fltk-dialog
-fi
 
